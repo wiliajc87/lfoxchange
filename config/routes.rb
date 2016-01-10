a5621d7b2cf62c833ec760c6a95493157c3992e7
Jordy Williams <jordythepoet@gmail.com>
2016-01-09 23:53:55 -0600

    add session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :answers, only: [:new, :create, :update, :destroy]
+  resources :answers, only: [:new, :create, :update, :destroy, :edit]
   resources :questions
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
