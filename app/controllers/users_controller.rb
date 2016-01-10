8d428370ca0c192c910088ae162b31498f80c320
Jordy Williams <jordythepoet@gmail.com>
2016-01-09 22:58:57 -0600

    gotta permit the password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,6 +51,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name)
+      params.require(:user).permit(:name, :password)
     end
 end
