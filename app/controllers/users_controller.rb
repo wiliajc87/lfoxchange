898307dbb45ccda7a07e3a30ed029c93e623f198
Jordy Williams <jordythepoet@gmail.com>
2016-01-09 23:19:49 -0600

    mass add for sessions and dealing with them

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :current_user
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :answers
+  resources :answers, only: [:new, :create, :update, :destroy]
   resources :questions
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root 'questions#index'
 
