d52f16ea38feb589f67de5ae3744ad6c36513b2c
Jordy Williams <jordythepoet@gmail.com>
2016-01-16 16:04:40 -0600

    nesting answers into questions--should have been done earlier

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  resources :answers, only: [:new, :create, :update, :destroy, :edit]
-  resources :questions
+  root 'questions#index'
+
+  resources :questions do
+    resources :answers do
+        put 'like', to: 'answers#upvote'
+        put 'dislike', to: 'answers#downvote'
+      end
+    end
+
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
 
-  root 'questions#index'
 
 end
