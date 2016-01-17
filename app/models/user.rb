afa39d1de90a779aa27af2d6c56837c92bffd40d
Jordy Williams <jordythepoet@gmail.com>
2016-01-17 11:16:06 -0600

    wanted to display User's Questions

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
     validates :name, presence: :true, uniqueness: true
     validates :password, length: { minimum: 8}, on: :create
     has_secure_password
+    acts_as_voter
 end
